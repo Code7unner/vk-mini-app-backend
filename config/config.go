66088e4b23f139780eff95d799aa13781d10d30c
Code7unner <Cherbadzhimaxim@gmail.com>
2020-11-29 02:01:38 +1000

    Add heroku deploy

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ type Config struct {
 	Port      int    `env:"DB_PORT" envDefault:"5432"`
 	DbName    string `env:"DB_NAME,required"`
 	SslEnable bool   `env:"DB_SSL_ENABLE" envDefault:"false"`
-	DbString  string `env:"DB_STRING,required"`
+	DbString  string `env:"DATABASE_URL,required"`
 
 	// App config
 	LogLevel   string `env:"LOG_LEVEL" envDefault:"debug"`
